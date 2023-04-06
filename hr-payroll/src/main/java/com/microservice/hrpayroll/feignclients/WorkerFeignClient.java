57b07c5a10b3ea7234cdbcfa05f8e214fb137b1a
jonas ribeiro <jonasribeiro41@yahoo.com.br>
2023-04-06 18:45:08 -0300

    load balancer with Ereka client

--- hr-payroll/src/main/java/com/microservice/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/feignclients/WorkerFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservice.hrpayroll.entities.Worker;
 
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 
 	@GetMapping(value = "/{id}")
