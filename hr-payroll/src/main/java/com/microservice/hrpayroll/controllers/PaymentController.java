b26ba3f0b3d0bd2816b9894a9918c2b035cff465
jonas ribeiro <jonasribeiro41@yahoo.com.br>
2023-04-08 13:47:02 -0300

    Circuit Breaker with Resilience4j - fault tolerance

--- hr-payroll/src/main/java/com/microservice/hrpayroll/controllers/PaymentController.java
+++ hr-payroll/src/main/java/com/microservice/hrpayroll/controllers/PaymentController.java
@@ -10,16 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservice.hrpayroll.entities.Payment;
 import com.microservice.hrpayroll.services.PaymentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentController {
 
+	
 	@Autowired
 	PaymentService servive;
 
+	@CircuitBreaker(name = "default", fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
-	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+		public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = servive.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+
+	public ResponseEntity<Payment> getPaymentAlternative(Exception ex) {
+		Payment payment = new Payment("Brann", 400.0, 3);
 		return ResponseEntity.ok(payment);
 	}
 }
